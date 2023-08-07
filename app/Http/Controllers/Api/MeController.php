5d0150a6980367a738125d4d2be1dc346f8ab276
Mart <m@rtin.win>
2023-08-07 23:24:01 +0200

    Added return type to me api

--- app/Http/Controllers/Api/MeController.php
+++ app/Http/Controllers/Api/MeController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Traits\JsonResponses;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MeController extends Controller
 {
     use JsonResponses;
 
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
         return $this->success(['me' => new UserResource($request->user())]);
     }
