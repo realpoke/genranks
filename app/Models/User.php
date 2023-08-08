201071e72b9a38cfdb7a9de32cdc2ae373b18a07
Mart <m@rtin.win>
2023-08-08 23:37:54 +0200

    Added nickname to user fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail, FilamentUser
 
     protected $fillable = [
         'name',
+        'nickname',
         'email',
         'password',
         'monthly_elo',
