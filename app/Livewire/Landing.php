e90421d38a4ae3b4df0a90ffa9d18cae73253f6a
Mart <m@rtin.win>
2023-08-12 15:43:25 +0200

    Example countdown timer for command

--- app/Livewire/Landing.php
+++ app/Livewire/Landing.php
@@ -2,9 +2,37 @@
 
 namespace App\Livewire;
 
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Landing extends Component
 {
-    //
+    public $targetTime;
+
+    public $error = false;
+
+    public function mount()
+    {
+        app()->make(Kernel::class);
+        $schedule = app()->make(Schedule::class);
+
+        $tasks = collect($schedule->events());
+
+        $matches = $tasks->filter(function ($item) {
+            return Str::contains($item->command, 'gentool:fetch');
+        });
+
+        $event = $matches->first();
+
+        if ($event) {
+            $time = $matches->first()->nextRunDate();
+        } else {
+            $time = now();
+            $this->error = true;
+        }
+
+        $this->targetTime = $time;
+    }
 }
