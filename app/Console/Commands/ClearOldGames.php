7c69afc47c22f8b94ea3b5ecdbc810d96423bcd1
Mart <m@rtin.win>
2023-08-21 11:34:05 +0200

    Added ClearOldGames command

---
+++ app/Console/Commands/ClearOldGames.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Game;
+use App\Models\GameUser;
+use Illuminate\Console\Command;
+
+class ClearOldGames extends Command
+{
+    protected $signature = 'game:clear';
+
+    protected $description = 'Delete 1 day old games without verification.';
+
+    public function handle()
+    {
+        $oldGames = Game::notVerified()->where('updated_at', '<=', now()->addDays(-1));
+        $oldGamesCount = $oldGames->count();
+
+        // Detach all users from old games before deleting the games
+        GameUser::whereIn('game_id', $oldGames->pluck('id'))->delete();
+
+        $oldGames->delete();
+
+        $this->info('Cleared '.$oldGamesCount.' old games!');
+    }
+}
