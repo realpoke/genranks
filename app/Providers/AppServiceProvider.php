407b15874a3380363105c5b1db4501e7f4368538
Mart <m@rtin.win>
2023-08-21 09:58:33 +0200

    Added types to whereLike macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -74,25 +74,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Builder::macro('whereLike', function ($attributes, string $searchTerm) {
-            $this->where(function (Builder $query) use ($attributes, $searchTerm) {
+        Builder::macro('whereLike', function (array|string $attributes, string $searchTerm): Builder {
+            return $this->where(function (Builder $query) use ($attributes, $searchTerm) {
                 foreach (Arr::wrap($attributes) as $attribute) {
                     $query->when(
                         str_contains($attribute, '.'),
-                        function (Builder $query) use ($attribute, $searchTerm) {
+                        function (Builder $query) use ($attribute, $searchTerm): void {
                             [$relationName, $relationAttribute] = explode('.', $attribute);
-                            $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $searchTerm) {
+                            $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $searchTerm): void {
                                 $query->where($relationAttribute, 'LIKE', "%{$searchTerm}%");
                             });
                         },
-                        function (Builder $query) use ($attribute, $searchTerm) {
+                        function (Builder $query) use ($attribute, $searchTerm): void {
                             $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
                         }
                     );
                 }
             });
-
-            return $this;
         });
     }
 }
