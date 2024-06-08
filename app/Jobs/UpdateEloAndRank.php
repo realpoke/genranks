8e8953345664d124e3f34a02a0b8ddea7d6ecc1c
realpoke <m@rtin.win>
2024-06-08 03:08:20 +0200

    Added elo and rank updating job

---
+++ app/Jobs/UpdateEloAndRank.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Contracts\CalculatesElo;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateEloAndRank implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected User $playerA;
+
+    protected User $playerB;
+
+    protected bool $playerAWon;
+
+    public function __construct(User $playerA, User $playerB, bool $playerAWon)
+    {
+        $this->playerA = $playerA;
+        $this->playerB = $playerB;
+        $this->playerAWon = $playerAWon;
+    }
+
+    public function handle(CalculatesElo $eloCalculator)
+    {
+        $newRatings = $eloCalculator($this->playerA, $this->playerB, $this->playerAWon);
+
+        if ($newRatings->isEmpty()) {
+            return;
+        }
+
+        // TODO: Database transaction this so if one fails both should be reverted
+        $this->playerA->newElo($newRatings->get('playerANewElo'));
+        $this->playerB->newElo($newRatings->get('playerBNewElo'));
+    }
+}
