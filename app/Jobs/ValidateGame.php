b7dd0e8ca4dc7410eda26d6644179163a5dcddd5
realpoke <m@rtin.win>
2024-06-16 07:25:05 +0200

    Added game validation logging

--- app/Jobs/ValidateGame.php
+++ app/Jobs/ValidateGame.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class ValidateGame implements ShouldQueue
 {
@@ -29,6 +30,7 @@ class ValidateGame implements ShouldQueue
      */
     public function handle(ValidatesGameContract $validator): void
     {
+        Log::debug('Starting game validation: '.$this->game->hash);
         $validated = $validator($this->game);
     }
 }
