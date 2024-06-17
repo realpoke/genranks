0a08814e617d07a1df9c010c3b074188f8b87cd4
realpoke <m@rtin.win>
2024-06-17 10:27:10 +0200

    Dump game data on game page

--- resources/views/livewire/show-game.blade.php
+++ resources/views/livewire/show-game.blade.php
@@ -1,7 +1,8 @@
 <x-layouts.container>
     <x-sections.card>
         <div class="flex justify-between">
-            <h1 class="text-4xl text-gray-900 dark:text-gray-100">Blank</h1>
+            <h1 class="text-4xl text-gray-900 dark:text-gray-100">{{ $game->players[0]['Name'] ?? 'UNKNOWN' }} VS
+                {{ $game->players[1]['Name'] ?? 'UNKNOWN' }}</h1>
             <div class="mt-1 flex items-center gap-x-1.5">
                 <div class="flex-none h-2 w-2 rounded-full {{ $game->status->classes() }}">
                     <div
@@ -11,12 +12,17 @@
                 <p class="text-xs leading-5 text-gray-500">{{ $game->status }}</p>
             </div>
         </div>
-        <p class="flex mt-1 text-xs leading-5 text-gray-500">
-            {{ $game->hash ?: 'Generating hash...' }}
+        <p class="flex justify-between mt-1 text-xs leading-5 text-gray-500">
+            <span class="font-extrabold">{{ collect(explode('/', $game->meta['MapFile']))->last() }}</span>
+            <span>{{ $game->hash ?: 'Generating hash...' }}</span>
         </p>
     </x-sections.card>
 
     <x-sections.card class="pt-14">
-        <p>TEST</p>
+        <ul class="space-y-20">
+            @foreach ($game->summary as $summary)
+                <li>{{ collect($summary) }}</li>
+            @endforeach
+        </ul>
     </x-sections.card>
 </x-layouts.container>
