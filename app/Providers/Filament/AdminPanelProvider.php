67186128020c45e8ce49ca2a8d979b84579856d0
realpoke <m@rtin.win>
2024-06-22 06:22:09 +0200

    Removed spa mode

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->spa()
+            //->spa()
             ->id('admin')
             ->path('admin')
             ->colors([
