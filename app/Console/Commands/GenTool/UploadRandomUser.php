f3bf10e24724111632f16f3fed78066245fb7e40
realpoke <m@rtin.win>
2024-06-23 08:11:20 +0200

    Made command loopable

--- app/Console/Commands/GenTool/UploadRandomUser.php
+++ app/Console/Commands/GenTool/UploadRandomUser.php
@@ -18,7 +18,7 @@ class UploadRandomUser extends Command
      *
      * @var string
      */
-    protected $signature = 'gentool:uploadrandom';
+    protected $signature = 'gentool:uploadrandom {--loop : Loop infinitely}';
 
     /**
      * The console command description.
@@ -36,74 +36,76 @@ class UploadRandomUser extends Command
         Gets1v1GenToolGamesContract $gameGetter,
         SearchesForGenToolUserContract $userSearcher,
     ) {
-        // Get random user from today.
-        $users = $userGetter(Carbon::now());
-        if ($users->isEmpty()) {
-            $this->info('No users found.');
+        do {
+            // Get random user from today.
+            $users = $userGetter(Carbon::now());
+            if ($users->isEmpty()) {
+                $this->info('No users found.');
 
-            return;
-        }
-
-        $userValue = $users->random();
-        $userKey = $users->search($userValue);
-        $this->info('Processing user: '.$userKey);
-
-        // Find 1v1 replays and store opponents
-        $games = $gameGetter($userValue);
-        if ($games->isEmpty()) {
-            $this->info('No 1v1 games found.');
-
-            return;
-        }
-
-        // Create or find fake user
-        $user = $userCreator($userKey)->first();
-        $this->info('User created: '.$userKey);
-
-        $opponents = collect();
-        foreach ($games as $replayName => $replayURL) {
-            $this->info($replayName);
-            $segments = explode('_', $replayName);
-            $opponent = $segments[2] != explode('_', $userKey)[0] ? $segments[2] : $segments[3];
-            $opponents->add(preg_replace('/[^A-Za-z0-9]/', '', $opponent));
-
-            $fileContent = file_get_contents($replayURL);
-            $fileName = $user->id.'_'.time().'_'.$opponents->count().'_replay';
-            $uploadedReplay = Storage::disk('replays')->put($fileName, $fileContent);
-
-            if ($uploadedReplay != false) {
-                $this->info('Uploaded replay: '.$fileName);
-                ProcessReplay::dispatch($user, $fileName);
+                break;
             }
-        }
 
-        // Get replays from opponents
-        foreach ($userSearcher(Carbon::now(), ...$opponents->unique()->toArray()) as $userNickname => $userUrl) {
-            $this->info($userNickname);
-            // Find 1v1 replays and store
-            $opponentGames = $gameGetter($userUrl);
-            if ($opponentGames->isEmpty()) {
-                $this->info('No opponent 1v1 games found.');
+            $userValue = $users->random();
+            $userKey = $users->search($userValue);
+            $this->info('Processing user: '.$userKey);
 
-                return;
+            // Find 1v1 replays and store opponents
+            $games = $gameGetter($userValue);
+            if ($games->isEmpty()) {
+                $this->info('No 1v1 games found.');
+
+                break;
             }
-            $userOpponent = $userCreator($userNickname)->first();
-            $this->info('Opponent created: '.$userNickname);
 
-            $replayNumber = 0;
-            foreach ($opponentGames as $OpponentReplayName => $opponentReplayURL) {
-                $replayNumber++;
-                $this->info($OpponentReplayName);
+            // Create or find fake user
+            $user = $userCreator($userKey)->first();
+            $this->info('User created: '.$userKey);
 
-                $fileContent = file_get_contents($opponentReplayURL);
-                $fileName = $userOpponent->id.'_'.time().'_'.$replayNumber.'_replay';
+            $opponents = collect();
+            foreach ($games as $replayName => $replayURL) {
+                $this->info($replayName);
+                $segments = explode('_', $replayName);
+                $opponent = $segments[2] != explode('_', $userKey)[0] ? $segments[2] : $segments[3];
+                $opponents->add(preg_replace('/[^A-Za-z0-9]/', '', $opponent));
+
+                $fileContent = file_get_contents($replayURL);
+                $fileName = $user->id.'_'.time().'_'.$opponents->count().'_replay';
                 $uploadedReplay = Storage::disk('replays')->put($fileName, $fileContent);
 
                 if ($uploadedReplay != false) {
-                    $this->info('Uploaded opponent replay: '.$fileName);
-                    ProcessReplay::dispatch($userOpponent, $fileName);
+                    $this->info('Uploaded replay: '.$fileName);
+                    ProcessReplay::dispatch($user, $fileName);
                 }
             }
-        }
+
+            // Get replays from opponents
+            foreach ($userSearcher(Carbon::now(), ...$opponents->unique()->toArray()) as $userNickname => $userUrl) {
+                $this->info($userNickname);
+                // Find 1v1 replays and store
+                $opponentGames = $gameGetter($userUrl);
+                if ($opponentGames->isEmpty()) {
+                    $this->info('No opponent 1v1 games found.');
+
+                    break 2;
+                }
+                $userOpponent = $userCreator($userNickname)->first();
+                $this->info('Opponent created: '.$userNickname);
+
+                $replayNumber = 0;
+                foreach ($opponentGames as $OpponentReplayName => $opponentReplayURL) {
+                    $replayNumber++;
+                    $this->info($OpponentReplayName);
+
+                    $fileContent = file_get_contents($opponentReplayURL);
+                    $fileName = $userOpponent->id.'_'.time().'_'.$replayNumber.'_replay';
+                    $uploadedReplay = Storage::disk('replays')->put($fileName, $fileContent);
+
+                    if ($uploadedReplay != false) {
+                        $this->info('Uploaded opponent replay: '.$fileName);
+                        ProcessReplay::dispatch($userOpponent, $fileName);
+                    }
+                }
+            }
+        } while ($this->option('loop'));
     }
 }
