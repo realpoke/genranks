1438b0c9a4a6ac84bf72e40e9dc2f32ac1846ec9
realpoke <m@rtin.win>
2024-06-24 09:13:25 +0200

    Simple game stats page

--- app/Livewire/ShowGame.php
+++ app/Livewire/ShowGame.php
@@ -3,19 +3,82 @@
 namespace App\Livewire;
 
 use App\Models\Game;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class ShowGame extends Component
 {
     public Game $game;
 
+    public array $categories;
+
+    public Collection $users;
+
+    public array $comparisonData = [];
+
     public function mount(Game $game)
     {
         $this->game = $game;
+        $this->users = $game->users()->get();
     }
 
     public function render()
     {
+        $this->categories = ['UnitsCreated', 'BuildingsBuilt', 'UpgradesBuilt', 'PowersUsed'];
+        $this->comparisonData = $this->computeComparisonData($this->categories);
+
         return view('livewire.show-game');
+    }
+
+    #[Computed()]
+    private function computeComparisonData(array $categories): array
+    {
+        $comparisonData = [];
+
+        foreach ($categories as $category) {
+            $firstData = $this->game->summary[0][$category];
+            $secondData = $this->game->summary[1][$category];
+
+            // Ensure $firstData and $secondData are arrays
+            if (! is_array($firstData) || ! is_array($secondData)) {
+                continue; // Skip if not array (or handle differently based on your logic)
+            }
+
+            $firstCollection = collect($firstData);
+            $secondCollection = collect($secondData);
+
+            // Summing 'TotalSpent' and 'Count' if available, otherwise summing integers directly
+            $firstTotalSpent = $firstCollection->sum(function ($item) {
+                return is_array($item) && isset($item['TotalSpent']) ? $item['TotalSpent'] : 0;
+            });
+            $firstTotalCount = $firstCollection->sum(function ($item) {
+                return is_array($item) && isset($item['Count']) ? $item['Count'] : $item;
+            });
+
+            $secondTotalSpent = $secondCollection->sum(function ($item) {
+                return is_array($item) && isset($item['TotalSpent']) ? $item['TotalSpent'] : 0;
+            });
+            $secondTotalCount = $secondCollection->sum(function ($item) {
+                return is_array($item) && isset($item['Count']) ? $item['Count'] : $item;
+            });
+
+            $spentDiff = $secondTotalSpent - $firstTotalSpent;
+            $countDiff = $secondTotalCount - $firstTotalCount;
+
+            $totalSpentPercentage = $firstTotalSpent > 0 ? ($spentDiff / $firstTotalSpent) * 100 : 0;
+            $totalCountPercentage = $firstTotalCount > 0 ? ($countDiff / $firstTotalCount) * 100 : 0;
+
+            $comparisonData[$category] = [
+                'firstTotalSpent' => $firstTotalSpent,
+                'secondTotalSpent' => $secondTotalSpent,
+                'totalSpentPercentage' => $totalSpentPercentage,
+                'firstTotalCount' => $firstTotalCount,
+                'secondTotalCount' => $secondTotalCount,
+                'totalCountPercentage' => $totalCountPercentage,
+            ];
+        }
+
+        return $comparisonData;
     }
 }
--- resources/views/livewire/show-game.blade.php
+++ resources/views/livewire/show-game.blade.php
@@ -1,28 +1,178 @@
-<x-layouts.container>
-    <x-sections.card>
-        <div class="flex justify-between">
-            <h1 class="text-4xl text-gray-900 dark:text-gray-100">{{ $game->players[0]['Name'] ?? 'UNKNOWN' }} VS
-                {{ $game->players[1]['Name'] ?? 'UNKNOWN' }}</h1>
-            <div class="mt-1 flex items-center gap-x-1.5">
-                <div class="flex-none h-2 w-2 rounded-full {{ $game->status->classes() }}">
+<div class="overflow-hidden bg-gray-900 isolate">
+    <div class="px-6 pt-24 mx-auto text-center max-w-7xl pb-96 sm:pt-32 lg:px-8">
+        <div class="max-w-4xl mx-auto">
+            <h2 class="text-base font-semibold leading-7 text-indigo-400">
+                {{ collect(explode('/', $game->meta['MapFile']))->last() }}</h2>
+            <p class="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
+                {{ $game->summary[0]['Name'] }} VS {{ $game->summary[1]['Name'] }}</p>
+        </div>
+        <div class="relative mt-6">
+            <p class="max-w-2xl mx-auto text-lg leading-8 text-white/60">
+                {{ $game->hash }}</p>
+            <svg viewBox="0 0 1208 1024"
+                class="absolute -top-10 left-1/2 -z-10 h-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:-top-12 md:-top-20 lg:-top-12 xl:top-0">
+                <ellipse cx="604" cy="512" fill="url(#6d1bd035-0dd1-437e-93fa-59d316231eb0)" rx="604"
+                    ry="512" />
+                <defs>
+                    <radialGradient id="6d1bd035-0dd1-437e-93fa-59d316231eb0">
+                        <stop stop-color="#7775D6" />
+                        <stop offset="1" stop-color="#E935C1" />
+                    </radialGradient>
+                </defs>
+            </svg>
+        </div>
+    </div>
+    <div class="flow-root pb-24 bg-white sm:pb-32">
+        <div class="-mt-80">
+            <div class="px-6 mx-auto max-w-7xl lg:px-8">
+                <div class="grid max-w-md grid-cols-1 gap-8 mx-auto lg:max-w-4xl lg:grid-cols-2">
                     <div
-                        class="p-2 -ml-1 -mt-1 rounded-full {{ $game->status->animation() }} {{ $game->status->classes() }}">
+                        class="{{ $game->status == 'valid' && $game->summary[0]['Win'] == true ? 'border-l-2 border-r-2 border-indigo-600' : '' }} flex flex-col justify-between p-8 bg-white shadow-xl rounded-3xl ring-1 ring-gray-900/10 sm:p-10">
+                        <div>
+                            <h3 id="tier-hobby"
+                                class="flex items-center text-base font-semibold leading-7 text-indigo-600">
+                                @if ($game->status == 'valid' && $game->summary[0]['Win'] == true)
+                                    <x-icons icon="award" />
+                                @endif
+                                {{ $game->summary[0]['Side'] }}
+                            </h3>
+
+                            @if ($game->status == 'valid')
+                                <div class="flex items-baseline mt-4 gap-x-2">
+                                    <span
+                                        class="text-5xl font-bold tracking-tight text-gray-900">{{ abs($users->first()->pivot->elo_change) }}</span>
+                                    <div>
+                                        @if ($game->summary[0]['Win'])
+                                            <span class="text-base font-semibold leading-7 text-gray-600"><x-icons
+                                                    class="text-green-500" icon="trend-up" /></span>
+                                        @else
+                                            <span class="text-base font-semibold leading-7 text-gray-600"><x-icons
+                                                    class="text-red-500" icon="trend-down" /></span>
+                                        @endif
+                                        <p>/elo</p>
+                                    </div>
+                                </div>
+                            @endif
+                            <p class="mt-2 text-xs text-gray-600">
+                                {{ $users->first()->name }}<br>
+
+                                @if ($game->status == 'valid')
+                                    Rank: {{ $users->first()->rank }}/{{ $users->first()->elo }}
+                                @endif
+                            </p>
+                            <ul role="list" class="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                                @foreach ($categories as $category)
+                                    <li class="flex flex-col gap-y-3">
+                                        <div class="font-bold">{{ $category }}</div>
+
+                                        <div class="flex items-center gap-x-3">
+                                            @if ($comparisonData[$category]['totalSpentPercentage'] > 25)
+                                                <x-icons class="text-red-500" icon="trend-down" />
+                                            @elseif ($comparisonData[$category]['totalSpentPercentage'] < -25)
+                                                <x-icons class="text-green-500" icon="trend-up" />
+                                            @else
+                                                <x-icons class="text-gray-600" icon="activity" />
+                                            @endif
+                                            Total Spent: {{ $comparisonData[$category]['firstTotalSpent'] }}
+                                        </div>
+
+                                        <div class="flex items-center gap-x-3">
+                                            @if ($comparisonData[$category]['totalCountPercentage'] > 25)
+                                                <x-icons class="text-red-500" icon="trend-down" />
+                                            @elseif ($comparisonData[$category]['totalCountPercentage'] < -25)
+                                                <x-icons class="text-green-500" icon="trend-up" />
+                                            @else
+                                                <x-icons class="text-gray-600" icon="activity" />
+                                            @endif
+                                            Total Count: {{ $comparisonData[$category]['firstTotalCount'] }}
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                    <div
+                        class="{{ $game->status == 'valid' && $game->summary[1]['Win'] == true ? 'border-l-2 border-r-2 border-indigo-600' : '' }} flex flex-col justify-between p-8 bg-white shadow-xl rounded-3xl ring-1 ring-gray-900/10 sm:p-10">
+                        <div>
+
+                            <h3 id="tier-team"
+                                class="flex items-center text-base font-semibold leading-7 text-indigo-600">
+                                @if ($game->status == 'valid' && $game->summary[1]['Win'] == true)
+                                    <x-icons icon="award" />
+                                @endif
+                                {{ $game->summary[1]['Side'] }}
+                            </h3>
+                            @if ($game->status == 'valid')
+                                <div class="flex items-baseline mt-4 gap-x-2">
+                                    <span
+                                        class="text-5xl font-bold tracking-tight text-gray-900">{{ abs($users->last()->pivot->elo_change) }}</span>
+                                    <div>
+                                        @if ($game->summary[1]['Win'])
+                                            <span class="text-base font-semibold leading-7 text-gray-600"><x-icons
+                                                    class="text-green-500" icon="trend-up" /></span>
+                                        @else
+                                            <span class="text-base font-semibold leading-7 text-gray-600"><x-icons
+                                                    class="text-red-500" icon="trend-down" /></span>
+                                        @endif
+                                        <p>/elo</p>
+                                    </div>
+                                </div>
+                            @endif
+                            <p class="mt-2 text-xs text-gray-600">
+                                {{ $users->last()->name }}<br>
+
+                                @if ($game->status == 'valid')
+                                    Rank: {{ $users->last()->rank }}/{{ $users->last()->elo }}
+                                @endif
+                            </p>
+                            <ul role="list" class="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                                @foreach ($categories as $category)
+                                    <li class="flex flex-col gap-y-3">
+                                        <div class="font-bold">{{ $category }}</div>
+
+                                        <div class="flex items-center gap-x-3">
+                                            @if ($comparisonData[$category]['totalSpentPercentage'] < -25)
+                                                <x-icons class="text-red-500" icon="trend-down" />
+                                            @elseif ($comparisonData[$category]['totalSpentPercentage'] > 25)
+                                                <x-icons class="text-green-500" icon="trend-up" />
+                                            @else
+                                                <x-icons class="text-gray-600" icon="activity" />
+                                            @endif
+                                            Total Spent: {{ $comparisonData[$category]['secondTotalSpent'] }}
+                                        </div>
+
+                                        <div class="flex items-center gap-x-3">
+                                            @if ($comparisonData[$category]['totalCountPercentage'] < -25)
+                                                <x-icons class="text-red-500" icon="trend-down" />
+                                            @elseif ($comparisonData[$category]['totalCountPercentage'] > 25)
+                                                <x-icons class="text-green-500" icon="trend-up" />
+                                            @else
+                                                <x-icons class="text-gray-600" icon="activity" />
+                                            @endif
+                                            Total Count: {{ $comparisonData[$category]['secondTotalCount'] }}
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div
+                        class="flex flex-col items-start p-8 gap-x-8 gap-y-6 rounded-3xl ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-2 lg:flex-row lg:items-center">
+                        <div class="lg:min-w-0 lg:flex-1">
+                            <h3 class="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Something wrong?
+                            </h3>
+                            <p class="mt-1 text-base leading-7 text-gray-600">If you encounter any issues or bugs
+                                while using the leaderboard, please contact our support team. Provide as
+                                much detail as possible to help us
+                                resolve the issue promptly.</p>
+                        </div>
+                        <a href="{{ route('discord') }}"
+                            class="rounded-md px-3.5 py-2 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Discord
+                            Support</a>
                     </div>
                 </div>
-                <p class="text-xs leading-5 text-gray-500">{{ $game->status }}</p>
             </div>
         </div>
-        <p class="flex justify-between mt-1 text-xs leading-5 text-gray-500">
-            <span class="font-extrabold">{{ collect(explode('/', $game->meta['MapFile']))->last() }}</span>
-            <span>{{ $game->hash ?: 'Generating hash...' }}</span>
-        </p>
-    </x-sections.card>
-
-    <x-sections.card class="pt-14">
-        <ul class="space-y-20">
-            @foreach ($game->summary as $summary)
-                <li>{{ collect($summary) }}</li>
-            @endforeach
-        </ul>
-    </x-sections.card>
-</x-layouts.container>
+    </div>
+</div>
