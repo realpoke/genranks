1b69034f0218e483b4d88d19498635cad648f816
realpoke <m@rtin.win>
2024-07-08 02:16:38 +0200

    Added more game types

--- app/Enums/GameType.php
+++ app/Enums/GameType.php
@@ -10,6 +10,8 @@ enum GameType: string
 
     case ONE_ON_ONE = 'one-on-one';
     case TWO_ON_TWO = 'two-on-two';
+    case THREE_ON_THREE = 'three-on-three';
+    case FOUR_ON_FOUR = 'four-on-four';
 
     case FREE_FOR_ALL = 'free-for-all';
 
