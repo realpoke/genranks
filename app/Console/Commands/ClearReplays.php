9c41e4380723d9ca82b5802286c99c45699eb735
realpoke <m@rtin.win>
2024-07-16 01:07:51 +0200

    Only clear .rep files

--- app/Console/Commands/ClearReplays.php
+++ app/Console/Commands/ClearReplays.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ClearReplays extends Command
 {
@@ -30,6 +31,10 @@ class ClearReplays extends Command
         $fileNames = Storage::disk('replays')->files();
 
         foreach ($fileNames as $fileName) {
+            if (Str::endsWith($fileName, '.rep')) {
+                continue;
+            }
+
             $lastModified = Carbon::createFromTimestamp(Storage::disk('replays')->lastModified($fileName));
 
             if ($lastModified->diffInMinutes(Carbon::now()) > 60) {
