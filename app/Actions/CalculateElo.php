d7f3869ad96a74fe2a314d01030e22d604c6052f
realpoke <m@rtin.win>
2024-07-16 02:05:18 +0200

    Added ChangedElo to elo calculation

--- app/Actions/CalculateElo.php
+++ app/Actions/CalculateElo.php
@@ -3,6 +3,7 @@
 namespace App\Actions;
 
 use App\Contracts\CalculatesEloContract;
+use App\Enums\EloRankType;
 use App\Models\User;
 use Illuminate\Support\Collection;
 
@@ -10,17 +11,22 @@ class CalculateElo implements CalculatesEloContract
 {
     private int $kFactor = 32;
 
-    public function __invoke(User $playerA, User $playerB, bool $playerAWon): Collection
+    public function __invoke(User $playerA, User $playerB, bool $playerAWon, EloRankType $rankType = EloRankType::ALL): Collection
     {
-        $expectedA = 1 / (1 + pow(10, ($playerB->elo - $playerA->elo) / 400));
-        $expectedB = 1 / (1 + pow(10, ($playerA->elo - $playerB->elo) / 400));
+        $eloField = $rankType->databaseEloField();
 
-        $newRatingA = $playerA->elo + $this->kFactor * (($playerAWon ? 1 : 0) - $expectedA);
-        $newRatingB = $playerB->elo + $this->kFactor * (($playerAWon ? 0 : 1) - $expectedB);
+        $expectedA = 1 / (1 + pow(10, ($playerB->$eloField - $playerA->$eloField) / 400));
+        $expectedB = 1 / (1 + pow(10, ($playerA->$eloField - $playerB->$eloField) / 400));
+
+        $newRatingA = $playerA->$eloField + $this->kFactor * (($playerAWon ? 1 : 0) - $expectedA);
+        $newRatingB = $playerB->$eloField + $this->kFactor * (($playerAWon ? 0 : 1) - $expectedB);
 
         return collect([
             'playerANewElo' => round($newRatingA),
+            'playerAChangedElo' => round($newRatingA - $playerA->$eloField),
+
             'playerBNewElo' => round($newRatingB),
+            'playerBChangedElo' => round($newRatingB - $playerB->$eloField),
         ]);
     }
 }
