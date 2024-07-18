1a446dace5515a0c05bcb18dc54cfb65d9d7553f
realpoke <m@rtin.win>
2024-07-18 06:39:30 +0200

    First iteration of TeamWinnerProcessor

--- app/Actions/WinnerProcessor/TeamWinnerProcessor.php
+++ app/Actions/WinnerProcessor/TeamWinnerProcessor.php
@@ -4,6 +4,7 @@ namespace App\Actions\WinnerProcessor;
 
 use App\Contracts\Factory\WinnerProcessorContract;
 use App\Enums\GameStatus;
+use App\Jobs\GiveUserStats;
 use App\Models\Game;
 use Log;
 
@@ -13,10 +14,95 @@ class TeamWinnerProcessor implements WinnerProcessorContract
     {
         Log::debug('Team winner processor');
 
-        // TODO: Combine all team members elo into one for both teams
-        // then do a normal elo calculation on that like its 1v1
-        // Then take the lost and gained elo and divide it by the number of players on each team
+        // Order users same as game players field
+        $users = $game->users->sortBy(function ($user) {
+            return $user->pivot->header['ArrayReplayOwnerSlot'];
+        })->values();
+        Log::debug('Users: '.collect($users));
+        Log::debug('Users count: '.collect($users)->count());
+
+        $gamePlayers = collect($game->players);
+        Log::debug('Game players: '.collect($gamePlayers));
+        Log::debug('Players count: '.collect($gamePlayers)->count());
+
+        if ($users->count() !== $gamePlayers->count()) {
+            Log::error('Mismatch in number of users and game players for game: '.$game->id);
+
+            return GameStatus::INVALID;
+        }
+
+        $teams = $users->zip($gamePlayers)->groupBy(function ($pair) {
+            return $pair[1]['Team'];
+        })->filter(function ($team) {
+            return $team->count() > 0;
+        });
+        Log::debug('Teams: '.collect($teams));
+
+        if ($teams->count() !== 2) {
+            Log::error('Invalid number of teams for game: '.$game->id);
+
+            return GameStatus::INVALID;
+        }
+
+        // Plucking to just get the users
+        $team1 = $teams->first()->pluck(0);
+        $team2 = $teams->last()->pluck(0);
+        Log::debug('Team1: '.collect($team1));
+        Log::debug('Team2: '.collect($team2));
+
+        $team1Elo = $team1->avg('elo');
+        $team2Elo = $team2->avg('elo');
+        Log::debug('Team1 Elo: '.$team1Elo);
+        Log::debug('Team2 Elo: '.$team2Elo);
+
+        $team1Won = $team1->every(function ($user) {
+            return $user->pivot->summary['Win'] ?? false;
+        });
+        $team2Won = $team2->every(function ($user) {
+            return $user->pivot->summary['Win'] ?? false;
+        });
+        Log::debug('Team1 Won: '.$team1Won);
+        Log::debug('Team2 Won: '.$team2Won);
+
+        if (! $team1Won && ! $team2Won) {
+            return GameStatus::DRAW; // TODO: This should not happen, we should remove draw status
+        }
+
+        if ($team1Won && $team2Won) {
+            Log::debug('Both teams won. Game not valid');
+
+            return GameStatus::INVALID;
+        }
+
+        if ($game->map?->ranked) {
+            Log::debug('Ranked game');
+            Log::debug('Team1 won: '.$team1Won);
+            Log::debug('Team2 won: '.$team2Won);
+
+            // TODO: Remove when we have enough maps in the pool and added to the database seeder
+            $this->logMapDetails($game, 'Map already in pool and ranked');
+
+            // TODO: Calculate elo and update ranks / elo for all users in the game
+            GiveUserStats::dispatch($game);
+
+            return GameStatus::VALID;
+        } else {
+            Log::debug('Unranked game');
+
+            // TODO: Remove when we have enough maps in the pool and added to the database seeder
+            $this->logMapDetails($game, 'Map could be added');
+
+            return GameStatus::UNRANKED;
+        }
 
         return GameStatus::INVALID;
+    }
+
+    private function logMapDetails(Game $game, string $message): void
+    {
+        Log::debug($message.': '.$game->meta['MapHash']);
+        Log::debug('MapFile: '.$game->meta['MapFile']);
+        Log::debug('MapCRC: '.$game->meta['MapCRC']);
+        Log::debug('MapSize: '.$game->meta['MapSize']);
     }
 }
