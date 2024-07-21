6d4f3c652fea89044d50156dc80734627880d4d4
realpoke <m@rtin.win>
2024-07-21 03:15:16 +0200

    Added ZH images to profiles

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -4,6 +4,43 @@
         <p>Welcome to my profile!</p>
 
         <p>stats: {{ collect($user->stats) }}</p>
+
+        <div> {{-- TODO: Make this a component --}}
+            @foreach (collect($user->stats) as $stat => $value)
+                @if (in_array($stat, ['UnitsCreated', 'BuildingsBuilt', 'UpgradesBuilt', 'PowersUsed']))
+                    <div class="flex flex-col py-6">
+                        <h3 class="text-4xl">{{ $stat }}</h3>
+                    </div>
+                    <div class="grid grid-cols-2 gap-1 sm:grid-cols-4">
+                        @php
+                            if ($stat == 'PowersUsed') {
+                                $sortedValues = collect($value)->sortDesc();
+                            } else {
+                                $sortedValues = collect($value)->sortByDesc('TotalSpent');
+                            }
+                        @endphp
+                        @foreach ($sortedValues as $unit => $details)
+                            <div
+                                class="relative flex items-center space-x-3 bg-white border border-gray-300 shadow-sm hover:border-gray-400">
+                                <div class="flex-shrink-0">
+                                    <x-images.zh.icon :icon="$unit" :alt="$unit" />
+                                </div>
+                                <div class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate">{{ $unit }}</p>
+                                    <p class="text-sm text-gray-500 truncate">
+                                        @if ($stat == 'PowersUsed')
+                                            {{ 'Used ' . $details . ' times' }}
+                                        @else
+                                            {{ ($details['Count'] ?? '0') . ' worth ' . number_format($details['TotalSpent'] ?? 0) }}
+                                        @endif
+                                    </p>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            @endforeach
+        </div>
     </x-sections.card>
     <x-sections.card class="pt-12">
         <h3 class="text-2xl">My games:</h3>
