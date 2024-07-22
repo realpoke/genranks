966bf9d031ed7962773534b4f0cc8108b8c93cdd
realpoke <m@rtin.win>
2024-07-22 02:03:09 +0200

    Added alpha banner

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -16,6 +16,23 @@
     {{ $slot }}
 
     @stack('modals')
+
+    <div class="fixed inset-x-0 bottom-0 pointer-events-none sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
+        <div
+            class="pointer-events-auto flex items-center justify-between gap-x-6 bg-gray-900 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+            <p class="text-sm leading-6 text-white">
+                <strong class="font-semibold">ALPHA</strong><svg viewBox="0 0 2 2"
+                    class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
+                    <circle cx="1" cy="1" r="1" />
+                </svg>GenRanks is currently in alpha, data wipes are possible at any time. Questions? Reach out on
+                <a target="_blank" href="{{ route('discord') }}"
+                    class="inline-flex items-center font-semibold text-indigo-400 hover:text-indigo-300">Discord<x-icons
+                        icon="external-link" class="w-4 h-4" /></a>
+                </a>
+            </p>
+        </div>
+    </div>
+
 </body>
 
 </html>
