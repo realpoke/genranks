9f625d6df9b0ed4683234cebf23fc09db73dec2b
realpoke <m@rtin.win>
2024-07-28 17:22:48 +0200

    Validating game with ranked mode of players

--- app/Actions/ValidateGame.php
+++ app/Actions/ValidateGame.php
@@ -4,6 +4,7 @@ namespace App\Actions;
 
 use App\Contracts\ValidatesGameContract;
 use App\Enums\GameStatus;
+use App\Enums\RankMode;
 use App\Enums\Side;
 use App\Factories\WinnerProcessorFactory;
 use App\Models\Game;
@@ -11,6 +12,8 @@ use Illuminate\Support\Facades\Log;
 
 class ValidateGame implements ValidatesGameContract
 {
+    private const MAX_ELO_DIFFERENCE_FOR_BALANCED = 500;
+
     public function __invoke(Game $game): bool
     {
         if ($game->status != GameStatus::VALIDATING) {
@@ -25,6 +28,7 @@ class ValidateGame implements ValidatesGameContract
 
     private function validCheck(Game $game): GameStatus
     {
+        // Check if there are enough players
         if ($game->type->validPlayerCount(count($game->players)) === false) {
             Log::debug('Game type: '.$game->type->name.' - Invalid player count ('.count($game->players).'). Game not valid');
 
@@ -61,8 +65,49 @@ class ValidateGame implements ValidatesGameContract
 
     private function processWinner(Game $game): GameStatus
     {
+        // Check users ranked mode
+        $balanced = false;
+        foreach ($game->users as $user) {
+            if ($user->rank_mode == RankMode::ALL) {
+                continue;
+            } elseif ($user->rank_mode == RankMode::FUN) {
+                Log::debug('User playing for fun, unranked: '.$user->id);
+
+                return GameStatus::UNRANKED;
+            } elseif ($user->rank_mode == RankMode::BALANCED) {
+                Log::debug('User playing balanced, checking elo difference: '.$user->id);
+
+                $balanced = true;
+            }
+        }
+        if ($balanced === true) {
+            Log::debug('Game needs to be balanced, checking elo difference');
+
+            if ($this->checkBalanced($game) === GameStatus::UNRANKED) {
+                return GameStatus::UNRANKED;
+            }
+        }
+
         $processor = WinnerProcessorFactory::getProcessor($game->type);
 
         return $processor($game);
+    }
+
+    private function checkBalanced(Game $game): GameStatus
+    {
+        // Get the difference between the highest and lowest elo for all users in the game
+        $userElos = $game->users->pluck('elo')->toArray();
+
+        $highestElo = max($userElos);
+        $lowestElo = min($userElos);
+        $eloDifference = $highestElo - $lowestElo;
+
+        if ($eloDifference > self::MAX_ELO_DIFFERENCE_FOR_BALANCED) {
+            Log::debug('Elo difference too high, game not balanced, max allowed: '.self::MAX_ELO_DIFFERENCE_FOR_BALANCED.' elo difference: '.$eloDifference);
+
+            return GameStatus::INVALID;
+        }
+
+        return GameStatus::VALID;
     }
 }
